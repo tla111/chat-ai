fc0cb358eb9192f5370b740217a6e20f069294cf
Tim La <tlamarketing111@gmail.com>
2022-12-26 08:05:53 -0500

    app is able to listen to request

--- server/server.js
+++ server/server.js
@@ -44,3 +44,5 @@ app.post('/', async (req, res) => {
         res.status(500).send(error || 'Something went wrong');
     }
 })
+
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
